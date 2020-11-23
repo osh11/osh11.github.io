16a95c7dce6251528788a73b99dd2725e8b86054
osh11 <72486691+osh11@users.noreply.github.com>
2020-11-23 23:46:47 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: `ㅇㅇㅇㅇㅇ`,
+  title: `Ego`,
   description: `Blog posted about ...`,
   author: `[Soohyun]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  introduction: `오수현`,
+  siteUrl: `https://xkdlxkslr1997.netlify.app`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `osh11`, // Your GitHub account
@@ -15,13 +15,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'osh11/osh11.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'osh11',
+    buyMeACoffeeId: 'xkdlxkslr1997',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
